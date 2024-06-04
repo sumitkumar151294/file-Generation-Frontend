02c6749921079292cd9434de77dd0bd9cc9850eb
jatin2306 <jatingupta2306@gmail.com>
2024-06-04 22:20:29 +0530

    login page functionality

--- src/Layout/Sidebar/Sidebar.js
+++ src/Layout/Sidebar/Sidebar.js
@@ -1,130 +1,97 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
-// import { Link, useLocation, useNavigate } from "react-router-dom";
-// import { useDispatch, useSelector } from "react-redux";
-// import { allowModules, onGetModule, resetAllowModules } from "../../Store/Slices/moduleSlice";
-// import Loader from "../../Components/Loader/Loader";
-// import Logout from "../../Assets/img/Logout.png";
-// import { onLogout } from "../../Store/Slices/loginSlice";
-// import { GetTranslationData } from "../../Components/GetTranslationData/GetTranslationData ";
-// import { onGetUserRoleModuleAccess } from "../../Store/Slices/userRoleModuleAccessSlice";
+import React from "react";
+import logout from "../../Assets/img/Logout.png";
+import section from "../../Assets/img/section.png";
+import add from "../../Assets/img/Add.png";
+import content from "../../Assets/img/content.png";
+import segment from "../../Assets/img/segment.png";
+import dashboard from "../../Assets/img/dashboard.png";
+import { Link } from "react-router-dom";
 const Sidebar = () => {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-//   const [isSidebarLoading, setIsSidebarLoading] = useState(false);
-//   const [sideBarModules, setIsSideBarModules] = useState([]);
-//   const [selectedModuleId, setSelectedModuleId] = useState(null)
-//   const logout = GetTranslationData("UIAdmin", "logout");
-//   const loginDetails = useSelector((state) => state.loginReducer);
-//   const currentUrl = useLocation();
-//   // To reset the redux store (logout the user)
-//   const handleLogout = (e) => {
-//     e.preventDefault();
-//     dispatch(onLogout());
-//     localStorage.clear();
-//     sessionStorage.clear();
-//     loginDetails.partner_Key === "UIAdmin" ? navigate("/") : navigate("/lc-user-admin/login");
-//   };
-//   // get module data
-//   const getModuleData = useSelector((state) => state.moduleReducer);
-//     const userRoleModuleAccess = useSelector((state) => state.userRoleModuleAccessReducer?.data);
-//   const userRoleID = useSelector((state) => state.loginReducer?.data?.[0]?.adminRoleId);
-//   useEffect(() => {
-//     setIsSidebarLoading(true);
-//     dispatch(onGetModule());
-//     dispatch(onGetUserRoleModuleAccess());
-//     dispatch(resetAllowModules());
-//   }, []);
-
-//   useEffect(() => {
-//     if (!getModuleData.isLoading && userRoleModuleAccess.length > 0) {
-//       setIsSidebarLoading(false);
-//       let tempideModules = JSON.parse(JSON.stringify(getModuleData?.data));
-//       const filterData =userRoleModuleAccess.filter((item) => { return (item.roleId === userRoleID && (item.addAccess || item.editAccess || item.viewAccess)) });
-//       const filterModules = []
-//       for(var i=0; i<tempideModules.length; i++){
-//         for(var j=0; j<filterData?.length; j++){
-//           if(tempideModules[i].id===filterData[j].moduleId){
-//             tempideModules[i].moduleId = filterData[j].moduleId
-//             filterModules.push(tempideModules[i])
-//           }
-//         }
-//       }
-//       setIsSideBarModules(filterModules)
-//     } else {
-//       setIsSidebarLoading(true);
-//     }
-//   }, [getModuleData, userRoleModuleAccess]);
-
-
-
-//   // function to add active class on Li
-//   const hanleClick = (e, moduleId) => {
-//     document.querySelectorAll(".mm-active").forEach((e) => {
-//       e.classList.remove("mm-active");
-//     });
-//     e.target.closest(".nav-icn").classList.add("mm-active");
-//     setSelectedModuleId(moduleId);
-//     dispatch(resetAllowModules());
-//   };
-//   const getModuleDataAccess = userRoleModuleAccess.filter((item) => { return (item.roleId === userRoleID && (item.addAccess || item.editAccess || item.viewAccess)) });
-
-//  useEffect(() => {
-//   if (getModuleDataAccess && selectedModuleId !== null && !getModuleData?.filteredData?.length) {
-//     const roleAcessValues = getModuleDataAccess.filter(item => item.moduleId === selectedModuleId);
-//     dispatch(allowModules(roleAcessValues));
-//   }else if(getModuleDataAccess && selectedModuleId === null && !getModuleData?.filteredData?.length){
-//     const data = sideBarModules.find(item=>(item.routePath.toLowerCase() === currentUrl.pathname.toLowerCase()))
-//     const roleAcessValues = getModuleDataAccess.filter(item => item.moduleId === data?.moduleId);
-//     dispatch(allowModules(roleAcessValues));
-//   }
-// }, [userRoleModuleAccess, selectedModuleId, sideBarModules]);
-
   return (
     <div className="deznav">
-      {/* <div className="deznav-scroll mm-active ps ps--active-y">
-        {true ? (
-          <div style={{ height: "400px" }}>
-            <Loader classType={"absoluteLoader"} />
-          </div>
-        ) : (
-          <ul className="metismenu mm-show" id="menu">
-            {sideBarModules.length &&
-              sideBarModules?.map((item, index) => (
-                <li
-                  key={index}
-                  // className={`nav-icn ${
-                  //   item.routePath === currentUrl.pathname ? "mm-active" : ""
-                  //   }`}
-                  // onClick={(e) => hanleClick(e, item.id)}
-                >
-                  <Link
-                    className="ai-icon"
-                    // to={item.routePath}
-                    aria-expanded="false"
-                  >
-                    <img
-                      src={require(  `../../Assets/icon/${item.icon}.svg`)}
-                      alt={item.icon}
-                    />
-                    <span className="nav-text ps-1">{item.name}</span>
-                  </Link>
-                </li>
-              ))}
-            <li>
-              <Link
-                className="ai-icon "
-                onClick={handleLogout}
-                aria-expanded="false"
-              >
-                <img className="w-20px" src={Logout} alt="file not exist" />
-                <span className="nav-text ps-1"> {logout}</span>
-              </Link>
-            </li>
-          </ul>
-        )}
-      </div> */}
+      <div className="deznav-scroll mm-active ps ps--active-y">
+        <ul className="metismenu mm-show" id="menu">
+          <li
 
+          // className={`nav-icn ${
+          //   item.routePath === currentUrl.pathname ? "mm-active" : ""
+          //   }`}
+          // onClick={(e) => hanleClick(e, item.id)}
+          >
+            <Link
+              to="/dashboard"
+              className="ai-icon"
+              // to={item.routePath}
+              aria-expanded="false"
+            >
+              <img className="w-20px" src={dashboard} alt="file not exist" />
+
+              <span className="nav-text ps-1">Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/file-type"
+              className="ai-icon "
+              // onClick={handleLogout}
+              aria-expanded="false"
+            >
+              <img className="w-20px" src={segment} alt="file not exist" />
+              <span className="nav-text ps-1"> File Type</span>
+            </Link>
+          </li>{" "}
+          <li>
+            <Link
+              to="/variable-dictionary"
+              className="ai-icon "
+              aria-expanded="false"
+            >
+              <img className="w-20px" src={segment} alt="file not exist" />
+              <span className="nav-text ps-1"> Variable Dictionary</span>
+            </Link>
+          </li>{" "}
+          <li>
+            <Link
+              to="/client-master"
+              className="ai-icon "
+              aria-expanded="false"
+            >
+              {" "}
+              <img className="w-20px" src={content} alt="file not exist" />
+              <span className="nav-text ps-1"> Client Master</span>
+            </Link>
+          </li>{" "}
+          <li>
+            <Link
+              to="/templatetype-master"
+              className="ai-icon "
+              aria-expanded="false"
+            >
+              <img className="w-20px" src={section} alt="file not exist" />
+              <span className="nav-text ps-1"> Template Type Master</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="template-master"
+              className="ai-icon "
+              // onClick={handleLogout}
+              aria-expanded="false"
+            >
+              <img className="w-20px" src={add} alt="file not exist" />
+
+              <span className="nav-text ps-1"> Template Master</span>
+            </Link>
+          </li>
+          <li>
+            <Link className="ai-icon " aria-expanded="false">
+              <img className="w-20px" src={logout} alt="file not exist" />
+              <span className="nav-text ps-1"> Logout</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/Routing/routes.js
+++ src/Routing/routes.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from '../Layout/Layout'
 import FileTypeList from "../Pages/FileType/FileTypeList";
 import VariableDictionaryList from "../Pages/VariableDictionary/VariableDictionaryList";
@@ -10,8 +10,7 @@ function RouteConfiq() {
 
     <Router>
       <Routes>
-      {/* <Route path="/" element={<Login/>} /> */}
-
+      <Route path="/" element={<Login/>} />
         <Route path="/file-type" element={<Layout Component={FileTypeList} />} />
         <Route path="/variable-dictionary" element={<Layout Component={VariableDictionaryList} />} />
         <Route path="/client-master" element={<Layout Component={ClientMasterList} />} />
